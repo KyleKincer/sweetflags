84cdfa4de377906cc5cb44d7137363cb6f2d8285
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-05-05 14:31:26 -0400

    Password for Redis

--- src/redis.ts
+++ src/redis.ts
@@ -11,6 +11,7 @@ class RedisCache {
         this.client = new Redis({
             host: process.env.REDIS_HOST,
             port: Number(process.env.REDIS_PORT),
+            password: process.env.MEMORYSTORE_REDIS_PASSWORD,
         });
     }
 
