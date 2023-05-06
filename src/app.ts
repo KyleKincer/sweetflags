8b455a7e55803d7d84d51b9de097667f99fd3eca
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-05-06 15:32:36 -0400

    try again

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,17 @@ import 'dd-trace/init';
 dotenv.config();
 console.log('Starting server');
 
+// TEMPORARY MIGRATION TO CLEAR DATABASE-- REMOVE BEFORE PRODUCTION
+(async () => {
+  try {
+    await clearDatabase();
+    console.log('Database cleared successfully');
+  } catch (error) {
+    console.error('Error while clearing the database:', error);
+  }
+})();
 
 async function connectToDb() {
-  // TEMPORARY MIGRATION TO CLEAR DATABASE-- REMOVE BEFORE PRODUCTION
-  // await clearDatabase();
   try {
     if (!process.env.MONGODB_URI) {
       throw new Error('MONGODB_URI environment variable is not set');
