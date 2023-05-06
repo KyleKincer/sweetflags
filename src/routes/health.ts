06906705aea40fe68d254cb8e4cc82d7ea846e82
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-05-06 15:44:37 -0400

    Add health route to clear db

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).json({ message: 'OK' });
 });
+
+router.post('/cleardb', async (req, res) => {
+    try {
+        // Drop the database
+        await mongoose.connection.dropDatabase();
+
+        console.log('Database cleared.');
+    } catch (error) {
+        console.error('Error while clearing the database:', error);
+    }
+});
+
 
 export default router;
