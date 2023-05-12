e7e1a9ea8a5824f607c92fa1703c55826b5de114
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-05-12 10:01:12 -0500

    Initial material implementation with homepage update

--- sweetflags-ui/src/main.ts
+++ sweetflags-ui/src/main.ts
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
 import './index.css';
 import App from './App.vue'
-import router from './router';
+import router from './router'
 import { Collapse } from 'vue-collapsed';
 import { auth0 } from './auth/auth';
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(App).component('Collapse', Collapse).use(router).use(auth0);
+loadFonts()
 
-app.mount('#app')
+const app = createApp(App)
+    .component('Collapse', Collapse)
+    .use(router)
+    .use(auth0)
+    .use(vuetify);
+
+app.mount('#app');
