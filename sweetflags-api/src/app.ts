43a85e08a02ba8ff2dac788f3b0e4078130e61e4
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-11-15 11:46:08 -0500

    Attempt to fix vercel deploy

--- sweetflags-api/src/app.ts
+++ sweetflags-api/src/app.ts
@@ -16,41 +16,39 @@ import 'dd-trace/init';
 dotenv.config();
 console.log('Starting server');
 
-async function connectToDb() {
-  try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error('MONGODB_URI environment variable is not set');
-    }
-
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to database');
-  } catch (err) {
-    console.error('Error connecting to database:', err);
-    throw err;
+try {
+  if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI environment variable is not set');
   }
 
-  // Add index on flags collection
-  await mongoose.connection.db.collection('flags').createIndex({ app: 1, name: 1 });
-
-  const app = express();
-
-  const port = process.env.PORT || 3000;
-  app.set('port', port);
-
-  // Set up Swagger UI
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use('/api/healthz', healthRouter);
-  app.use('/api/flags', flagRouter);
-  app.use('/api/apps', appRouter);
-  app.use('/api/environments', environmentRouter);
-  app.use('/api/users', userRouter);
-  app.use('/api/logs', logRouter);
-
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to database');
+} catch (err) {
+  console.error('Error connecting to database:', err);
+  throw err;
 }
 
-connectToDb();
+// Add index on flags collection
+await mongoose.connection.db.collection('flags').createIndex({ app: 1, name: 1 });
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+// Set up Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/healthz', healthRouter);
+app.use('/api/flags', flagRouter);
+app.use('/api/apps', appRouter);
+app.use('/api/environments', environmentRouter);
+app.use('/api/users', userRouter);
+app.use('/api/logs', logRouter);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
